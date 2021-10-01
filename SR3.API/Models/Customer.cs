f7ef7cbf8057a32094df0e5f4433646112fa0f82
samoeun chhay <samoeunchh@gmail.com>
2021-10-01 15:18:49 +0700

    Add project files.

---
+++ SR3.API/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR3.API.Models
+{
+    public class Customer
+    {
+        [Key]
+        public Guid CustomerId { get; set; }
+        [Required(ErrorMessage ="Customer name is required")]
+        [MaxLength(50)]
+        public string CustomerName { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string Gender { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Mobile { get; set; }
+        [MaxLength(100)]
+        public string Address { get; set; }
+    }
+}
